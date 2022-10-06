74aafff4553efafe4df561a9c136fd7fb060c498
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-10-06 12:31:29 +0100

    Add points and levels

--- SimpleSnake/SimpleSnake/GameObjects/Field.cs
+++ SimpleSnake/SimpleSnake/GameObjects/Field.cs
@@ -9,17 +9,19 @@ namespace SimpleSnake.GameObjects
     public class Field : Point
     {
         private const char WallSymbol = '\u25A0';
+        private int playerPoints;
 
         public Field(int leftX, int topY)
             : base(leftX, topY)
         {
             this.InitializeWallBorder();
+            this.PlayerInfo();
         }
 
         public bool IsPointOfWall(Point snakeHead)
         {
-            return snakeHead.LeftX == 0 || snakeHead.TopY == 0 ||
-                   snakeHead.LeftX == this.LeftX - 1 || snakeHead.TopY == this.TopY;
+            return snakeHead.LeftX == 0 || snakeHead.LeftX == this.LeftX - 1 || 
+                   snakeHead.TopY == 0  || snakeHead.TopY == this.TopY;
         }
 
         private void InitializeWallBorder()
@@ -45,6 +47,20 @@ namespace SimpleSnake.GameObjects
             {
                 this.Draw(leftX, topY, WallSymbol);
             }
+        }
+
+        public void AddPoints(Queue<Point> snakeElements)
+        {
+            this.playerPoints = snakeElements.Count - 6;
+        }
+
+        public void PlayerInfo()
+        {
+            Console.SetCursorPosition(this.LeftX + 3, 0);
+            Console.Write($"Player points: {this.playerPoints}");
+
+            Console.SetCursorPosition(this.LeftX + 3, 1);
+            Console.Write($"Player level: {this.playerPoints / 10}");
         }
     }
 }
